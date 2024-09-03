4c4ee0228b794673f9f355958f2e05213afd8e74
EGARRISXN <egarrisxn@gmail.com>
2024-09-03 04:46:24 -0400

    added pacman!

--- app/(pages)/contact/page.jsx
+++ app/(pages)/contact/page.jsx
@@ -2,6 +2,7 @@ import Link from '../../components/link'
 import ContactForm from '../../components/contact-form'
 import {Arrow} from '../../components/icons/other'
 import Socials from '../../components/socials'
+import Pacman from '../../components/pacman'
 
 export const metadata = {
   title: 'Contact',
@@ -17,6 +18,7 @@ export default function ContactPage() {
       <header>
         <h3>Contact me _</h3>
       </header>
+      <Pacman />
       <p>
         If you have any questions, comments, or interest in working together, go ahead and send over
         a messsage and I&apos;ll get back with you as soon as I can.
