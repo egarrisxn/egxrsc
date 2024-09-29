9aad8a41b489bd3575f70fd3de1b6c722a8ab3ce
EGARRISXN <egarrisxn@gmail.com>
2024-09-28 20:57:12 -0400

    updated some projects on here

--- app/lib/get-projects.jsx
+++ app/lib/get-projects.jsx
@@ -2,6 +2,15 @@ import {cache} from 'react'
 
 const Projects = [
   {
+    title: 'Sway Bae *Official*',
+    description:
+      'The place to be for full-time streamer and content creator, Sway Bae. Created exactly as she wanted using Next.js, Sanity, Tailwind CSS, daisyUI, & deployed with Vercel.',
+    href: 'https://github.com/egarrisxn/swaybaeofficial',
+    years: ['2023', '2024'],
+    type: 'project',
+    image: '/projects/swaybae.png',
+  },
+  {
     title: 'Unofficial Merchandise',
     description:
       'A fully functional e-commerce store built with users in mind! Leveraging modern web teb technologies like Next.js, Sanity, Stripe, Tailwind CSS, shadcn/ui, & deployed with Vercel.',
@@ -11,7 +20,7 @@ const Projects = [
     image: '/projects/unofficialmerch.png',
   },
   {
-    title: 'n00b:dev',
+    title: 'n00bdev - Tips & Tricks',
     description:
       'A cutting-edge, server-side rendered blog designed by developers, for developers. Built with the Astro Framework, TypeScript, Tailwind CSS, & deployed with Vercel.',
     href: 'https://github.com/egarrisxn/n00bdev',
@@ -20,7 +29,7 @@ const Projects = [
     image: '/projects/n00bdev.png',
   },
   {
-    title: 'WebSanity Builder',
+    title: 'WebSanity: Website Builder',
     description:
       'A powerful, user-friendly tool designed to help you create stunning websites effortlessly! Powered by Next.js, Sanity, Tailwind CSS, & deployed with Vercel.',
     href: 'https://github.com/egarrisxn/websanity-builder',
@@ -29,22 +38,22 @@ const Projects = [
     image: '/projects/websanity.png',
   },
   {
-    title: 'EG Movies',
-    description:
-      'An intuitive movie search engine offering everything you would expect from a comprehensive movie database. Built with Angular, TypeScript, Tailwind CSS, & more.',
-    href: 'https://github.com/egarrisxn/eg-angular-movies',
-    years: ['2024'],
-    type: 'project',
-    image: '/projects/egmovies.png',
-  },
-  {
-    title: 'ATS Resbuilder',
+    title: 'Resbuilder | ATS-Certified',
     description:
       'A simple and professional ATS-Certified resume template and more using the likes of Next.js, JavaScript, MDX, Tailwind CSS, & deployed with Vercel.',
     href: 'https://github.com/egarrisxn/resume-builder',
     years: ['2024'],
     type: 'project',
     image: '/projects/resumebuilder.png',
+  },
+  {
+    title: 'EG // MOVIES DB',
+    description:
+      'An intuitive movie search engine offering everything you would expect from a comprehensive movie database. Built with Angular, TypeScript, Tailwind CSS, & more.',
+    href: 'https://github.com/egarrisxn/eg-angular-movies',
+    years: ['2024'],
+    type: 'project',
+    image: '/projects/egmovies.png',
   },
   {
     title: 'Devbuild Resume',
@@ -56,25 +65,7 @@ const Projects = [
     image: '/projects/devbuildresume.png',
   },
   {
-    title: 'Sway Bae Official',
-    description:
-      'The official website for a full-time streamer and content creator, Sway Bae. Created exactly as the client visioned by using Next.js, Sanity, Tailwind CSS, daisyUI, & deployed with Vercel.',
-    href: 'https://github.com/egarrisxn/swaybaeofficial',
-    years: ['2023'],
-    type: 'project',
-    image: '/projects/swaybae.png',
-  },
-  {
-    title: 'EGXBLOG',
-    description:
-      'My *old* personal blog I created as an attempt to write a little more. Spun up by the new Next.js 14, Sanity, Tailwind CSS, shadcn/ui, & deployed with Vercel.',
-    href: 'https://github.com/egarrisxn/egxblog',
-    years: ['2023'],
-    type: 'project',
-    image: '/projects/egxblog.png',
-  },
-  {
-    title: 'EGXWORLD V1',
+    title: 'EGXWORLD v1',
     description:
       'My first portfolio / website / whatever you would like to call it. Powered by Next.js, Tailwind CSS, shadcn/ui, & deployed with Vercel.',
     href: 'https://github.com/egarrisxn/egxworld-v1',
@@ -83,16 +74,25 @@ const Projects = [
     image: '/projects/egxworldv1.png',
   },
   {
-    title: 'm3rnb00kstack',
+    title: 'EGXBLOG | `Inside My Head` ',
     description:
-      'A MERN Stack book application, showcasing REST APIs (CRUD), authorization & authentication, etc. Tech: Express, MongoDB, React, JavaScript, Tailwind CSS, & deployed with Render.',
-    href: 'https://github.com/egarrisxn/m3rn-b00k-stack',
+      'My personal blog I created as an attempt to write a little more. Spun up by the new Next.js 14, Sanity, Tailwind CSS, shadcn/ui, & deployed with Vercel.',
+    href: 'https://github.com/egarrisxn/egxblog',
     years: ['2023'],
     type: 'project',
-    image: '/projects/m3rnb00kstack.png',
+    image: '/projects/egxblog.png',
   },
   {
-    title: 'The Tech Forum',
+    title: '(PEHN) User Management',
+    description:
+      'A simple CRUD application for organizing a user database and uses the powers of Pug, Express, HTMX, & Node.js together to function so smoothly. Deployed with Heroku.',
+    href: 'https://github.com/egarrisxn/pehn-user-management',
+    years: ['2023'],
+    type: 'project',
+    image: '/projects/pehnusers.png',
+  },
+  {
+    title: 'Tech Forum 2023',
     description:
       'A forum application designed in a CMS style, demonstrating the CRUD principles. This app is built with Express, MySQL, Handlebars, Bootstrap, & deployed with Heroku.',
     href: 'https://github.com/egarrisxn/mvc-tech-forum',
@@ -101,7 +101,25 @@ const Projects = [
     image: '/projects/mvctechforum.png',
   },
   {
-    title: 'Vanilla Template',
+    title: 'M3RN B00K STACK',
+    description:
+      'A MERN Stack book application, showcasing REST APIs (CRUD), authorization & authentication, etc. Tech: Express, MongoDB, React, JavaScript, Tailwind CSS, & deployed with Render.',
+    href: 'https://github.com/egarrisxn/m3rn-b00k-stack',
+    years: ['2023'],
+    type: 'project',
+    image: '/projects/m3rnb00kstack.png',
+  },
+  {
+    title: 'Theme Party w/ Vite',
+    description:
+      'A fun, no frills, little theme selector project, made with Vanilla JavaScript, CSS3, HTML5, Vite, and deployed with Netlify.',
+    href: 'https://github.com/egarrisxn/vite-theme-party',
+    years: ['2023'],
+    type: 'project',
+    image: '/projects/vitethemeparty.png',
+  },
+  {
+    title: 'HTML Portfolio Template',
     description:
       'A modern and beautiful portfolio template using only the powers from the trinity: HTML5, CSS3, & JavaScript. Deployed on GitHub Pages',
     href: 'https://github.com/egarrisxn/portfolio-template',
